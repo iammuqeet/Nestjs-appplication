0ed3ddda8997d307181f068bed8b92882580f6ae
iammuqeet <iammuqeetahmad@gmail.com>
2023-11-07 23:47:45 +0500

    Implemented the CRUD using nestjs

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Patch } from '@nestjs/common';
 import { ProductsService } from './products.service';
+import { Product } from './products.model';
 
 @Controller('products')
 export class ProductsController {
@@ -11,5 +12,28 @@ export class ProductsController {
   ): any {
     const generatedId = this.productsService.insertProducts(name, description);
     return { id: generatedId };
+  }
+
+  @Get()
+  getAllProducts() {
+    return this.productsService.getAllProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param() params: any): Product | string {
+    return this.productsService.getProduct(params);
+  }
+
+  @Patch('update/:id')
+  updateProduct(
+    @Param('id') productId: string,
+    @Body('name') productName: string,
+    @Body('description') productDescription: string,
+  ) {
+    return this.productsService.updateProduct(
+      productId,
+      productName,
+      productDescription,
+    );
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,53 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './products.model';
 
 @Injectable()
 export class ProductsService {
-  products: Product[] = [];
+  private products: Product[] = [];
 
   insertProducts(name: string, description: string) {
     const randomId = new Date().getTime().toString();
     const newProducts = new Product(randomId, name, description);
     this.products.push(newProducts);
     return randomId;
+  }
+
+  getAllProducts() {
+    return [...this.products];
+  }
+
+  getProduct(params) {
+    const product = this.findProduct(params.id);
+
+    if (product) {
+      return { ...product };
+    } else {
+      throw new NotFoundException(
+        `Product with the ${params.id} does not exist`,
+      );
+    }
+  }
+
+  updateProduct(productId: string, productName, productDescription): Product {
+    const product = this.findProduct(productId);
+    const productIndex = this.products.findIndex(
+      (product) => product.id === productId,
+    );
+
+    if (product) {
+      const updateProduct = { ...product };
+      if (productName) updateProduct.name = productName;
+      if (productDescription) updateProduct.description = productDescription;
+      this.products[productIndex] = updateProduct;
+      return updateProduct;
+    } else {
+      throw new NotFoundException(`Product with the ${product}`);
+    }
+  }
+
+  private findProduct(productId) {
+    const product = this.products.find((product) => product.id === productId);
+    if (product) return product;
+    else throw new NotFoundException('Product not found');
   }
 }
